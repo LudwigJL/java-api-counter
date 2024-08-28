3742cf371187dd4e2c732bf76be9fa43f0b9c164
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-08-28 12:48:17 +0200

    Core finished

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,5 +1,32 @@
 package com.booleanuk.api.counter;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CounterController {
+    private int counter;
+
+    CounterController(){
+        counter = 0;
+    }
+    @GetMapping("counter")
+    public String getCounter(){
+        return "" + this.counter;
+    }
+
+    @GetMapping("counter/increment")
+    public String incrementCounter(){
+        counter++;
+
+        return "" + counter;
+    }
+
+    @GetMapping("counter/decrement")
+    public String decrementCounter(){
+        counter--;
+
+        return "" + counter;
+    }
 
 }
